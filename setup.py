9aad74531b40e6c7685f00acbb5e177a08e14021
MarioDEFRANCE <mario.defrance@ca-lf.com>
2023-01-05 19:52:55 +0100

    Updating to version 1.0.3

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name='AutoCarver',
-    version='1.0.2',
+    version='1.0.3',
     author='Mario DEFRANCE',
     author_email='defrancemario@gmail.com',
     description='Automatic Carving of Features with Optimal Association',
