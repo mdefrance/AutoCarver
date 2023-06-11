9b7fc33e6b5a4f881073699c1596e169aa78d62c
MarioDEFRANCE <74489838+mdefrance@users.noreply.github.com>
2023-06-11 11:36:05 +0200

    test github actions pypi publish

--- setup.py
+++ setup.py
@@ -1,11 +1,15 @@
-import setuptools
+from codecs import open
+from os import path
+from setuptools import setup
 
-with open("README.md", "r", encoding="utf-8") as fh:
-    long_description = fh.read()
+here = path.abspath(path.dirname(__file__))
 
-setuptools.setup(
+with open(path.join(here, "README.md"), encoding="utf-8") as fh:
+    long_description = "\\n" + fh.read()
+
+setup(
     name='AutoCarver',
-    version='4.3.2',
+    version='{{VERSION_PLACEHOLDER}}',
     author='Mario DEFRANCE',
     author_email='defrancemario@gmail.com',
     description='Automatic Bucketizing of Features with Optimal Association',
@@ -18,13 +22,16 @@ setuptools.setup(
     license='MIT',
     packages=['AutoCarver'],
     classifiers=[
-        'Development Status :: 3 - Alpha',  # ou 4 - Beta ou 5 - Production/Stable
+        'Development Status :: 4 - Beta',  # ou 4 - Beta ou 5 - Production/Stable
         'License :: OSI Approved :: MIT License',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
         'Programming Language :: Python :: 3.10',
-        'Programming Language :: Python :: 3.11'
+        'Programming Language :: Python :: 3.11',
+        "Operating System :: Unix",
+        # "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows"
     ],
     python_requires='>=3.7'
 )
