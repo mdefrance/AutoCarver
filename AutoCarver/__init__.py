349b9aab8ce7ceb01547bead947c648d7f27a63b
MarioDEFRANCE <74489838+mdefrance@users.noreply.github.com>
2023-11-01 12:21:54 +0100

    import selectors directly

--- AutoCarver/__init__.py
+++ AutoCarver/__init__.py
@@ -4,7 +4,5 @@ from .carvers.base_carver import BaseCarver, load_carver
 from .carvers.binary_carver import AutoCarver, BinaryCarver
 from .carvers.continuous_carver import ContinuousCarver
 from .carvers.multiclass_carver import MulticlassCarver
-from . import selectors
-from . import carvers
-from . import discretizers
-from . import converters
+from .selectors.classification_selector import ClassificationSelector
+from .selectors.regression_selector import RegressionSelector
