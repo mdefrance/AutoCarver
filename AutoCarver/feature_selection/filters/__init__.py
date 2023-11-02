97be4201fbb4eebd1f02d001439c669d74e005a8
MarioDEFRANCE <74489838+mdefrance@users.noreply.github.com>
2023-11-02 14:07:59 +0100

    corrected location of measures and filters in old inits

--- AutoCarver/feature_selection/filters/__init__.py
+++ AutoCarver/feature_selection/filters/__init__.py
@@ -1,5 +1,5 @@
 """ Loads association filters."""
 
-from .base_filters import thresh_filter
-from .qualitative_filters import cramerv_filter, tschuprowt_filter
-from .quantitative_filters import pearson_filter, spearman_filter
+from ...selectors.filters.base_filters import thresh_filter
+from ...selectors.filters.qualitative_filters import cramerv_filter, tschuprowt_filter
+from ...selectors.filters.quantitative_filters import pearson_filter, spearman_filter
