d4b5cb5ce6ac4a9bbb1259e753a28dfdeb477890
MarioDEFRANCE <74489838+mdefrance@users.noreply.github.com>
2023-11-02 14:10:26 +0100

    done linting and removed non released imports

--- AutoCarver/selectors/__init__.py
+++ AutoCarver/selectors/__init__.py
@@ -1,7 +1,9 @@
 """ Loads feature selection tools."""
 
 from .base_selector import *
-from .classification_selector import ClassificationSelector
+
+# from .classification_selector import ClassificationSelector
 from .filters import *
 from .measures import *
-from .regression_selector import RegressionSelector
+
+# from .regression_selector import RegressionSelector
