00f05ee6ca7b7e776d82cccf68d361d293b7478c
MarioDEFRANCE <74489838+mdefrance@users.noreply.github.com>
2023-11-19 10:07:00 +0100

    linting

--- AutoCarver/carvers/multiclass_carver.py
+++ AutoCarver/carvers/multiclass_carver.py
@@ -15,7 +15,7 @@ from .binary_carver import BinaryCarver
 class MulticlassCarver(BaseCarver):
     """Automatic carving of continuous, discrete, categorical and ordinal
     features that maximizes association with a multiclass target.
-    
+
     Examples
     --------
     `Multiclass Classification Example <https://autocarver.readthedocs.io/en/latest/examples/MulticlassClassification/multiclass_classification_example.html>`_
@@ -39,9 +39,8 @@ class MulticlassCarver(BaseCarver):
         verbose: bool = False,
         **kwargs,
     ) -> None:
-        """
-        """
-        
+        """ """
+
         # association measure used to find the best groups for multiclass targets
         implemented_measures = ["tschuprowt", "cramerv"]
         assert sort_by in implemented_measures, (
