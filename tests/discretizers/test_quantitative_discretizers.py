b4ef9769a638f5a447262fab42fb38b8f4303d0f
MarioDEFRANCE <74489838+mdefrance@users.noreply.github.com>
2024-05-09 12:02:59 +0200

    fixed continuous disc test

--- tests/discretizers/test_quantitative_discretizers.py
+++ tests/discretizers/test_quantitative_discretizers.py
@@ -3,11 +3,11 @@
 from numpy import inf
 from pandas import DataFrame
 
-from AutoCarver.config import NAN
 from AutoCarver.discretizers import ContinuousDiscretizer
+from AutoCarver.features import Features
 
 
-def test_quantile_discretizer(x_train: DataFrame):
+def test_continuous_discretizer(x_train: DataFrame):
     """Tests ContinuousDiscretizer
 
     Parameters
@@ -16,47 +16,50 @@ def test_quantile_discretizer(x_train: DataFrame):
         Simulated Train DataFrame
     """
 
-    features = [
+    quantitatives = [
         "Quantitative",
         "Discrete_Quantitative",
         "Discrete_Quantitative_highnan",
         "Discrete_Quantitative_lownan",
         "Discrete_Quantitative_rarevalue",
     ]
+    features = Features(quantitatives=quantitatives)
+    for feature in features:
+        feature.dropna = True
     min_freq = 0.1
 
     discretizer = ContinuousDiscretizer(
         features,
         min_freq,
         copy=True,
-        n_jobs=1,
     )
     x_discretized = discretizer.fit_transform(x_train)
 
     assert all(
         x_discretized.Quantitative.value_counts(normalize=True) == min_freq
     ), "Wrong quantiles"
-    assert discretizer.values_orders["Discrete_Quantitative_highnan"] == [
+    assert discretizer.features("Discrete_Quantitative_highnan").values == [
         2.0,
         3.0,
         4.0,
         7.0,
         inf,
-        NAN,
+        features("Discrete_Quantitative_highnan").nan,
     ], "NaNs should be added to the order"
     assert (
-        NAN in x_discretized["Discrete_Quantitative_highnan"].unique()
+        features("Discrete_Quantitative_highnan").nan
+        in x_discretized["Discrete_Quantitative_highnan"].unique()
     ), "NaNs should be filled with the str_nan value"
-    assert discretizer.values_orders["Discrete_Quantitative_lownan"] == [
+    assert discretizer.features("Discrete_Quantitative_lownan").values == [
         1.0,
         2.0,
         3.0,
         4.0,
         6.0,
         inf,
-        NAN,
+        features("Discrete_Quantitative_rarevalue").nan,
     ], "NaNs should not be grouped whatsoever"
-    assert discretizer.values_orders["Discrete_Quantitative_rarevalue"] == [
+    assert discretizer.features("Discrete_Quantitative_rarevalue").values == [
         0.5,
         1.0,
         2.0,
