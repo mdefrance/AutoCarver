fc68f349e9263542b82dbffd78da26e286d6c90d
MarioDEFRANCE <74489838+mdefrance@users.noreply.github.com>
2024-05-30 08:07:23 +0200

    fixed continuous carver

--- AutoCarver/carvers/continuous_carver.py
+++ AutoCarver/carvers/continuous_carver.py
@@ -9,6 +9,7 @@ from scipy.stats import kruskal
 from ..discretizers.utils.base_discretizer import extend_docstring
 from ..features import GroupedList
 from .utils.base_carver import BaseCarver
+from ..features import Features
 
 
 class ContinuousCarver(BaseCarver):
@@ -23,22 +24,18 @@ class ContinuousCarver(BaseCarver):
     Classification/multiclass_classification_example.html>`_
     """
 
+    __name__ = "ContinuousCarver"
+
     @extend_docstring(BaseCarver.__init__)
     def __init__(
         self,
         min_freq: float,
+        features: Features,
         *,
-        quantitative_features: list[str] = None,
-        qualitative_features: list[str] = None,
-        ordinal_features: list[str] = None,
-        values_orders: dict[str, GroupedList] = None,
         max_n_mod: int = 5,
         min_freq_mod: float = None,
         ordinal_encoding: bool = True,
         dropna: bool = True,
-        copy: bool = False,
-        verbose: bool = False,
-        n_jobs: int = 1,
         **kwargs: dict,
     ) -> None:
         """
@@ -46,28 +43,21 @@ class ContinuousCarver(BaseCarver):
         ----------
         """
         # association measure used to find the best groups for continuous targets
-        if "sort_by" in kwargs:
-            assert kwargs.get("sort_by") == "kruskal", (
-                f" - [ContinuousCarver] Measure '{kwargs.get('sort_by')}' not yet implemented for "
-                "continuous targets. Only 'kruskal' measure is implemented for continuous targets."
+        if "sort_by" in kwargs and kwargs.get("sort_by") != "kruskal":
+            raise ValueError(
+                f" - [{self.__name__}] Measure '{kwargs.get('sort_by')}' not implemented for "
+                "continuous targets. Use 'kruskal' instead."
             )
-            kwargs.pop("sort_by")
 
         # Initiating BaseCarver
         super().__init__(
             min_freq=min_freq,
             sort_by="kruskal",
-            quantitative_features=quantitative_features,
-            qualitative_features=qualitative_features,
-            ordinal_features=ordinal_features,
-            values_orders=values_orders,
+            features=features,
             max_n_mod=max_n_mod,
             min_freq_mod=min_freq_mod,
             ordinal_encoding=ordinal_encoding,
             dropna=dropna,
-            copy=copy,
-            verbose=verbose,
-            n_jobs=n_jobs,
             **kwargs,
         )
 
@@ -107,19 +97,18 @@ class ContinuousCarver(BaseCarver):
 
         # continuous target, checking values
         y_values = unique(y)
-        assert (
-            len(y_values) > 2
-        ), " - [ContinuousCarver] provided y is binary, consider using BinaryCarver instead."
-        not_numeric = str in y.apply(type).unique()
-        assert (
-            not not_numeric
-        ), " - [ContinuousCarver] y must be a continuous Series (int or float, not object)"
+        if len(y_values) <= 2:
+            raise ValueError(
+                f" - [{self.__name__}] provided y is binary, consider using BinaryCarver instead."
+            )
+        if str in y.apply(type).unique():
+            raise ValueError(
+                f" - [{self.__name__}] y must be a continuous Series (int or float, not object)"
+            )
 
         return x_copy, x_dev_copy
 
-    def _aggregator(
-        self, features: list[str], X: DataFrame, y: Series, labels_orders: dict[str, GroupedList]
-    ) -> dict[str, DataFrame]:
+    def _aggregator(self, X: DataFrame, y: Series) -> dict[str, DataFrame]:
         """Computes y values for modalities of specified features and ensures the ordering
         according to the known labels
 
@@ -140,22 +129,22 @@ class ContinuousCarver(BaseCarver):
             _description_
         """
         # checking for empty datasets
-        yvals = {feature: None for feature in features}
+        yvals = {feature.name: None for feature in self.features}
         if X is not None:
             # crosstab for each feature
-            for feature in features:
+            for feature in self.features:
                 # computing crosstab with str_nan
-                yval = y.groupby(X[feature]).apply(lambda u: list(u))  # pylint: disable=W0108
+                yval = y.groupby(X[feature.name]).apply(lambda u: list(u))  # pylint: disable=W0108
 
                 # reordering according to known_order
-                yval = yval.reindex(labels_orders[feature], fill_value=[])
+                yval = yval.reindex(feature.labels, fill_value=[])
 
                 # storing results
                 yvals.update({feature: yval})
 
         return yvals
 
-    def _grouper(self, yval: Series, groupby: dict[str:str]) -> Series:
+    def _grouper(self, xagg: Series, groupby: dict[str:str]) -> Series:
         """Groups values of y
 
         Parameters
@@ -171,14 +160,14 @@ class ContinuousCarver(BaseCarver):
             _description_
         """
         # TODO: convert this to the vectorial version like BinaryCarver
-        return yval.groupby(groupby).sum()
+        return xagg.groupby(groupby).sum()
 
-    def _association_measure(self, yval: Series, **kwargs) -> dict[str, float]:
+    def _association_measure(self, xagg: Series, n_obs: int) -> dict[str, float]:
         """Computes measures of association between feature and quantitative target.
 
         Parameters
         ----------
-        yval : DataFrame
+        xagg : DataFrame
             Values taken by y for each of x's modalities.
 
         Returns
@@ -186,17 +175,17 @@ class ContinuousCarver(BaseCarver):
         dict[str, float]
             Kruskal-Wallis' H as a dict.
         """
-        _ = kwargs  # unused attribute
+        _ = n_obs  # unused attribute
 
         # Kruskal-Wallis' H
-        return {"kruskal": kruskal(*tuple(yval.values))[0]}
+        return {"kruskal": kruskal(*tuple(xagg.values))[0]}
 
-    def _target_rate(self, yval: Series) -> Series:
+    def _target_rate(self, xagg: Series) -> Series:
         """Computes target average per group for a continuous target
 
         Parameters
         ----------
-        yval : Series
+        xagg : Series
             _description_
 
         Returns
@@ -204,14 +193,14 @@ class ContinuousCarver(BaseCarver):
         Series
             _description_
         """
-        return yval.apply(mean).sort_values()
+        return xagg.apply(mean).sort_values()
 
-    def _printer(self, yval: Series = None) -> DataFrame:
+    def _printer(self, xagg: Series = None) -> DataFrame:
         """Prints a continuous yval's statistics
 
         Parameters
         ----------
-        yval : Series
+        xagg : Series
             A series of values of y by modalities of x, by default None
 
         Returns
@@ -221,32 +210,15 @@ class ContinuousCarver(BaseCarver):
         """
         # checking for an xtab
         stats = None
-        if yval is not None:
+        if xagg is not None:
             # target rate and frequency statistics per modality
             stats = DataFrame(
                 {
                     # target rate per modality
-                    "target_rate": yval.apply(mean),
+                    "target_rate": xagg.apply(mean),
                     # frequency per modality
-                    "frequency": yval.apply(len) / yval.apply(len).sum(),
+                    "frequency": xagg.apply(len) / xagg.apply(len).sum(),
                 }
             )
 
         return stats
-
-    @extend_docstring(BaseCarver.fit)
-    def fit(
-        self,
-        X: DataFrame,
-        y: Series,
-        *,
-        X_dev: DataFrame = None,
-        y_dev: Series = None,
-    ) -> None:
-        # preparing datasets and checking for wrong values
-        x_copy, x_dev_copy = self._prepare_data(X, y, X_dev, y_dev)
-
-        # Fitting BaseCarver
-        super().fit(x_copy, y, X_dev=x_dev_copy, y_dev=y_dev)
-
-        return self
