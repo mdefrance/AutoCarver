d2f3f2d08bfc91cabec995c9d970366d86ca96df
MarioDEFRANCE <74489838+mdefrance@users.noreply.github.com>
2025-01-11 11:52:30 +0100

    fixed tests

--- tests/selectors/measures/test_quantitative_measures.py
+++ tests/selectors/measures/test_quantitative_measures.py
@@ -81,18 +81,21 @@ def test_quanti_binary_measure_type(quanti_binary_measure: BaseMeasure) -> None:
 
 @fixture
 def binary_series_data() -> Series:
+    """creates a series with binary data"""
     x = Series([1, 1, 0, 1, 0, 0, 0, 0, 0, 0])
     return x
 
 
 @fixture
 def quali_series_data() -> Series:
+    """creates a series with qualitative data"""
     x = Series([2, 2, 0, 1, 1, 1, 1, 0, 0, 0])
     return x
 
 
 @fixture
 def outlier_series_data() -> Series:
+    """creates a series with outliers"""
     x = Series(
         [
             1,
@@ -441,7 +444,7 @@ def test_outlier_validate_with_computed_association_above_threshold(
 
     # without nans
     outlier_measure.threshold = 0.02
-    outlier_measure.compute_association(outlier_series_data.replace(51, 5e50))
+    outlier_measure.compute_association(outlier_series_data.replace(51, 5e15))
     print(
         outlier_measure.__name__,
         outlier_measure.value,
@@ -451,6 +454,7 @@ def test_outlier_validate_with_computed_association_above_threshold(
     assert not outlier_measure.validate(), "kept feature with lower than threshold correlation"
 
     # with nans
+    print(outlier_series_data.dtype, outlier_series_data.apply(type).unique())
     outlier_measure.compute_association(outlier_series_data.replace(50, nan).replace(51, 5e50))
     print(
         outlier_measure.__name__,
